9f1a2035d33f54cde2a9b3e4e97880db0edcc845
Sushma Sri <sushmasri1906@gmail.com>
2024-05-04 13:32:56 +0530

    Homepage Update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,6 +5,7 @@ import Carousel from "./Carousel";
 import Cater from "./Cater";
 import Designs from "./Designs";
 import MiniProductsAndServices from "./MiniProductsAndServices";
+import ServicesSection from "./ServiceSection";
 
 function Home() {
 	return (
@@ -13,6 +14,7 @@ function Home() {
 			<MiniProductsAndServices />
 			<Designs/>
 			<Cater/>
+			<ServicesSection/>
 			
 			
 		</div>
---
+++ src/components/Home/ServiceSection.tsx
@@ -0,0 +1,56 @@
+const ServiceSection = () => {
+    const images = [
+        "https://www.polyplast.co.in/assets/img/service-icon-1.webp",
+        "https://www.polyplast.co.in/assets/img/service-icon-2.webp",
+        "https://www.polyplast.co.in/assets/img/service-icon-3.webp",
+        "https://www.polyplast.co.in/assets/img/service-icon-4.webp"
+    ];
+
+    const serviceTitles = [
+        "Installation",
+        "Inspection",
+        "Repair",
+        "Refurbishment"
+    ];
+
+    return (
+        <section id="hm-services" className="p-5  banner-services bg-gray-300">
+            <div className="container my-lg-5">
+                <div className="text-center text-3xl font-light mb-16">
+                    <h2>High Quality Services</h2>
+                    <h2>For Industries</h2>
+                </div>
+                <div className="flex flex-col md:flex-row justify-center items-center">
+                    <div className="md:w-1/3 mb-3 md:mb-0">
+                        <img
+                            src="https://www.polyplast.co.in/assets/img/services-img.webp"
+                            className="img-fluid h-80 " // Increased height and added animation
+                            alt="Services"
+                        />
+                    </div>
+                    <div className="md:w-1/3 ml-5 grid grid-cols-2 gap-3">
+                        {images.map((image, index) => (
+                            <div key={index} className="bg-gray-600 hover:bg-red-700 transition-colors duration-700 text-white text-center p-4 flex flex-col justify-center items-center mb-3"> {/* Adjusted Tailwind classes */}
+                                <img
+                                    src={image}
+                                    className="img-fluid h-16 mb-3" // Increased height
+                                    alt={`Service ${index + 1}`}
+                                />
+                                <h4 className="text-center">{serviceTitles[index]}</h4> {/* Centered the title */}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ServiceSection;
+
+
+
+
+
+
+
