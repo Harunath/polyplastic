4ff1b2f6c4d3e0d96371172cbeaf770decea6e70
Sushma Sri <sushmasri1906@gmail.com>
2024-05-06 11:39:42 +0530

    Aboutuspage Updates

--- src/components/AboutUs/WhyUs/Services.tsx
+++ src/components/AboutUs/WhyUs/Services.tsx
@@ -45,7 +45,7 @@ const Services = () => {
       <div className="flex flex-wrap justify-center">
         {servicesData.map((service) => (
           <div key={service.id} className="max-w-xs w-32 h-32 mx-2 my-4 pb-2 bg-white rounded-lg overflow-hidden shadow-md text-center relative">
-            <div className="w-16 h-16 mx-auto my-4 rounded-full border-4 border-gray-300 flex items-center justify-center overflow-hidden transition duration-300 ease-in-out transform hover:border-red-500">
+            <div className="w-16 h-16 mx-auto my-4 rounded-full border-8 border-gray-400 flex items-center justify-center overflow-hidden transition duration-300 ease-in-out transform hover:border-red-500 p-1">
               <img src={service.imageUrl} alt={`Service ${service.id}`} className="w-full h-full object-cover object-center p-2" />
             </div>
             <p className="text-gray-700 text-xs px-2 mb-2">{service.description}</p>
