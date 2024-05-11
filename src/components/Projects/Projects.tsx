c10dd935c2a4ce5b96fc3dc531f8204faa9ffa86
harunath <harunath04@gmail.com>
2024-05-11 14:07:07 +0530

    material small changes

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,13 @@
-import HeroBanner from "./HeroBanner";
+import TopBanner from "../TopBanner";
 import Intro from "./Intro";
 
 function Projects() {
 	return (
 		<div>
-			<HeroBanner />
+			<TopBanner
+				image="https://www.24hourgroup.com.mm/wp-content/uploads/2018/07/project_image-1024x517.png"
+				text="Project"
+			/>
 			<Intro />
 		</div>
 	);
